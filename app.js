10286a90b8f6440fc33681970ba41a311d46bf7b
matthew dehart <mattidehart@hotmail.com>
2022-07-26 16:37:24 -0400

    adding on

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
     }
-};
-  
-printProfileData(profileDataArgs);
+  ])
+  .then(answers => console.log(answers));
